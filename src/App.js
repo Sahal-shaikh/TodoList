6eab0a67e1a3a21bb835727aff3c95becc08def0
Sahal-shaikh <sahal.shaikh@reveation.io>
2024-06-18 21:47:23 +0530

    Todo with Basic Task

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Dashboard from './components/views/Dashboard';
+import Layout from './components/views/Layout';
+import { StoreProvider } from './hooks/useStore';
+import { NotificationProvider } from './hooks/notificationContext';
+import Todo from './components/views/Todo';
+import TaskManager from './components/views/TaskManager';
+import Profile from './components/views/Profile';
+import Login from './components/views/Login';
+import Signup from './components/views/SignUp';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<StoreProvider>
+			<NotificationProvider>
+				<div className="App">
+					<BrowserRouter>
+						<Routes>
+							<Route path="/" element={<Layout />}>
+								<Route index element={<Dashboard />} />
+								<Route path="/edit/:id" element={<Todo />} />
+								<Route path="/AddItem/Form" element={<TaskManager />} />
+								<Route path='/account/profile' element={<Profile />} />
+								<Route path="/login" element={<Login />} />
+								<Route path="/signup" element={<Signup />} />
+							</Route>
+						</Routes>
+					</BrowserRouter>
+				</div>
+			</NotificationProvider>
+		</StoreProvider>
+	);
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock('./hooks/useData');
+describe('App Component', () => {
+	it('renders learn react link', async () => {});
 });
